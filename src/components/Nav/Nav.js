bbe3c12ffa052bf44f04ea15b41164d64161551a
김진평 <jp@gimjinpyeong-ui-MacBookPro.local>
2023-03-26 22:36:36 +0900

    ADD : Navigation 커밋 MODIFY : 불필요한 import 제거 MODIFY : styled components 스타일 별도 파일로 분리 및 컴포넌트 네이밍 재정립 MODIFY : nav 컴포넌트 map 함수 내 구조분해 할당으로 코드 간소화 작업 진행

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
+import * as S from './Nav.style';
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const navigate = useNavigate();
+  const login_token = localStorage.getItem('login-token');
+
+  const logout = () => {
+    localStorage.removeItem('login-token');
+    navigate('/');
+  };
+  return (
+    <S.NavContainer>
+      <S.NavWrapper>
+        <S.Home>
+          <S.StyledHomeLink to="/">Wevre</S.StyledHomeLink>
+        </S.Home>
+        <S.MenuList>
+          {MENU_LIST.map(({ id, text, link }) => {
+            if (id === 2) {
+              return login_token ? (
+                <S.StyledMenuLink key={id} onClick={logout}>
+                  {text[1]}
+                </S.StyledMenuLink>
+              ) : (
+                <S.StyledLink key={id} to={link}>
+                  {text[0]}
+                </S.StyledLink>
+              );
+            } else {
+              return (
+                <S.StyledLink key={id} to={link}>
+                  {text}
+                </S.StyledLink>
+              );
+            }
+          })}
+          <S.StyledLink to="/search">
+            <FaSearch />
+          </S.StyledLink>
+        </S.MenuList>
+      </S.NavWrapper>
+    </S.NavContainer>
+  );
 };
 
 export default Nav;
+
+const MENU_LIST = [
+  { id: 1, text: '스토어', link: '/store' },
+  { id: 2, text: ['로그인', '로그아웃'], link: '/login' },
+  { id: 3, text: '마이페이지', link: '/mypage' },
+];
---
+++ src/components/Nav/Nav.style.js
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const NavContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  background-color: white;
+  width: 100%;
+  height: 140px;
+  border-bottom: 1px solid #e2ddd9;
+`;
+
+export const NavWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px 80px;
+  width: 1280px;
+`;
+
+export const Home = styled.div`
+  font-family: ${({ theme }) => theme.fontStyle.logo};
+`;
+
+export const MenuList = styled.ul`
+  display: flex;
+`;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 20px;
+  font-weight: 400;
+  margin: 0 20px;
+
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    color: black;
+  }
+`;
+
+export const StyledMenuLink = styled.li`
+  margin: 0 20px;
+  font-size: 20px;
+
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    cursor: pointer;
+    color: black;
+  }
+`;
+
+export const StyledHomeLink = styled(StyledLink)`
+  font-size: 48px;
+  margin: 0;
+`;
