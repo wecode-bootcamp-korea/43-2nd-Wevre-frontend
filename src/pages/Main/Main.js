345e67b13391fc3363c9aa0ca6828ca1f3acc107
김진평 <jp@gimjinpyeong-ui-MacBookPro.local>
2023-03-27 09:53:16 +0900

    ADD : main 페이지 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Banner from './Banner/Banner';
+import * as S from './Main.style';
+import introVideo from '../../assets/images/Main.mp4';
 
 const Main = () => {
-  return <div>Main</div>;
+  const [bannerData, setBannerData] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/mainIntro.json')
+      .then(res => res.json())
+      .then(data => setBannerData(data));
+  }, []);
+  return (
+    <S.MainContainer>
+      <S.IntroVideo>
+        <S.StyledVideo src={introVideo} autoPlay muted controls loop />
+      </S.IntroVideo>
+      <S.BannerWrapper>
+        {bannerData &&
+          bannerData.map(banner => <Banner key={banner.id} data={banner} />)}
+      </S.BannerWrapper>
+    </S.MainContainer>
+  );
 };
 
 export default Main;
---
+++ src/pages/Main/Main.style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  padding-top: 140px;
+`;
+
+export const IntroVideo = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: 100%;
+  background-color: black;
+`;
+
+export const StyledVideo = styled.video`
+  margin: 50px;
+  width: 90%;
+`;
+
+export const BannerWrapper = styled.div`
+  margin: 50px 0;
+`;
