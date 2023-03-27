6bcc99426dc20328e1650744459bce8bfa7e2e16
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-27 17:35:28 +0900

    Add : mypage 레이아웃 구현 및 컴포넌트 이동 구현 및 상수데이터 분리

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserInfo from './UserInfo/UserInfo';
+import Bid from './Bid/Bid';
+import BuyerQual from './BuyerQualification/BuyerQualification';
+import Purchase from './PurchaseHistory/PurchaseHistory';
+import SellerQual from './SellerQualification/SellerQualification';
+import { MYPAGE_CATEGORY } from './MYPAGE_CATEGORY';
+import * as S from './Mypage.style';
 
 const Mypage = () => {
-  return <div>Mypage</div>;
+  const [id, setId] = useState(0);
+
+  const ContentsComponents = {
+    0: <UserInfo />,
+    1: <Bid />,
+    2: <BuyerQual />,
+    3: <Purchase />,
+    4: <SellerQual />,
+  };
+
+  const Li = e => {
+    setId(e.target.id);
+  };
+
+  return (
+    <S.Section>
+      <S.Category>
+        <S.Ul>
+          {MYPAGE_CATEGORY.map(({ title }, id) => (
+            <S.Li onClick={Li} key={id} id={id}>
+              {title}
+            </S.Li>
+          ))}
+        </S.Ul>
+      </S.Category>
+      <S.Content>{ContentsComponents[id]}</S.Content>
+    </S.Section>
+  );
 };
 
 export default Mypage;
