aa9e3e1851e9e8c9c90925007f726d7687052b29
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-28 11:41:19 +0900

    Add: 회원정보페이지 구성

--- src/pages/Mypage/UserInfo/UserInfo.js
+++ src/pages/Mypage/UserInfo/UserInfo.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { MYPAGE_CATEGORY } from '../MYPAGE_CATEGORY';
+import * as S from './UserInfo.style';
 
-const UserInfo = () => {
-  return <div>UserInfo</div>;
+const UserInfo = ({ id }) => {
+  const title = MYPAGE_CATEGORY[id].title;
+
+  return (
+    <S.CategoryContainer>
+      <S.Title>{title}</S.Title>
+    </S.CategoryContainer>
+  );
 };
 
 export default UserInfo;
---
+++ src/pages/Mypage/UserInfo/UserInfo.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const CategoryContainer = styled.div`
+  padding: 50px;
+`;
+
+export const Title = styled.h1`
+  font-size: 25px;
+`;
