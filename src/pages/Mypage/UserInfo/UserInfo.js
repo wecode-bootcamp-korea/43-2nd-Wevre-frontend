ff1f21d0a7684d876abf4f41df8d974df7fd9c5d
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-28 14:40:14 +0900

    Modify: 수정사항반영

--- src/pages/Mypage/UserInfo/UserInfo.js
+++ src/pages/Mypage/UserInfo/UserInfo.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import { MYPAGE_CATEGORY } from '../MYPAGE_CATEGORY';
+import { SUB_TITLE } from '../SUB_TITLE';
+import BASE_URL from '../../../config';
+import { Avatar, Card, CardMedia } from '@mui/material';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import useFetch from '../../../hooks/useFetch';
 import * as S from './UserInfo.style';
 
-const UserInfo = ({ id }) => {
-  const title = MYPAGE_CATEGORY[id].title;
+const UserInfo = () => {
+  const { loading, data } = useFetch({ BASE_URL });
+  const piece = data;
 
   return (
     <S.CategoryContainer>
-      <S.Title>{title}</S.Title>
+      <S.Title>회원 정보</S.Title>
+      <S.UserInfoBox>
+        <S.UserInfoDetail>
+          <Avatar
+            alt="user name"
+            sx={{ width: 70, height: 70, marginRight: 5 }}
+          >
+            정인
+          </Avatar>
+          <S.UserInfoDetailBox>
+            <S.UserName>유정인</S.UserName>
+            <S.UserId>jaylenyu96@gmail.com</S.UserId>
+          </S.UserInfoDetailBox>
+        </S.UserInfoDetail>
+      </S.UserInfoBox>
+      <S.Wishlist>
+        <S.WishlistTitle>위시리스트</S.WishlistTitle>
+        <S.WishlistContainer>
+          {!loading &&
+            piece.map(piece => (
+              <Card key={piece.id}>
+                <S.BookMark>
+                  <BookmarkIcon />
+                </S.BookMark>
+                <CardMedia
+                  sx={{ width: 300, height: 300 }}
+                  component="img"
+                  image={piece.image}
+                />
+
+                <S.CardTitle>{piece.title}</S.CardTitle>
+                <S.CardArtist>{piece.artist}</S.CardArtist>
+              </Card>
+            ))}
+        </S.WishlistContainer>
+      </S.Wishlist>
     </S.CategoryContainer>
   );
 };
--- src/pages/Mypage/UserInfo/UserInfo.style.js
+++ src/pages/Mypage/UserInfo/UserInfo.style.js
@@ -1,9 +1,76 @@
+import theme from '../../../styles/theme';
 import styled from 'styled-components';
 
 export const CategoryContainer = styled.div`
+  ${theme.mixins.flexBox('center', 'center')}
+  flex-direction: column;
   padding: 50px;
+  height: 80%;
+  width: 60vw;
+`;
+
+export const UserInfoBox = styled.div`
+  ${theme.mixins.flexBox('center', 'center')}
+  height: 30%;
+  width: 100%;
+  margin-bottom: 50px;
 `;
 
 export const Title = styled.h1`
-  font-size: 25px;
+  font-size: ${({ theme }) => theme.sizes.large};
+  margin-bottom: 2%;
+`;
+
+export const UserInfoDetail = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const UserInfoDetailBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  line-height: 30px;
+`;
+
+export const UserName = styled.h2`
+  font-size: 20px;
+`;
+
+export const UserId = styled.h3`
+  font-size: 16px;
+`;
+
+export const Wishlist = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const WishlistTitle = styled.h1`
+  ${theme.mixins.flexBox('center', 'center')}
+  font-size: ${({ theme }) => theme.sizes.large};
+  margin-bottom: 2%;
+`;
+
+export const WishlistContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+`;
+
+export const BookMark = styled.div`
+  position: relative;
+  top: 10%;
+  left: 90%;
+  opacity: 0.9;
+`;
+
+export const CardTitle = styled.h2`
+  ${theme.mixins.flexBox('center', 'center')}
+  font-size: ${({ theme }) => theme.sizes.medium};
+  line-height: 30px;
+`;
+
+export const CardArtist = styled.h3`
+  ${theme.mixins.flexBox('center', 'center')}
+  font-size: ${({ theme }) => theme.sizes.small};
 `;
