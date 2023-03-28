c6319f01016638578ed7f747fbd0372ba566685c
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-28 15:42:15 +0900

    Add: 카테고리 리스트 라이브러리 적용

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -5,33 +5,55 @@ import BuyerQual from './BuyerQualification/BuyerQualification';
 import Purchase from './PurchaseHistory/PurchaseHistory';
 import SellerQual from './SellerQualification/SellerQualification';
 import { MYPAGE_CATEGORY } from './MYPAGE_CATEGORY';
+import { List, ListItemButton, ListItemText } from '@mui/material';
 import * as S from './Mypage.style';
 
 const Mypage = () => {
   const [id, setId] = useState(0);
 
   const ContentsComponents = {
-    0: <UserInfo />,
-    1: <Bid />,
-    2: <BuyerQual />,
-    3: <Purchase />,
-    4: <SellerQual />,
+    0: <UserInfo id={id} />,
+    1: <Bid id={id} />,
+    2: <BuyerQual id={id} />,
+    3: <Purchase id={id} />,
+    4: <SellerQual id={id} />,
   };
 
-  const Li = e => {
-    setId(e.target.id);
+  const handleCategory = id => {
+    setId(id);
   };
 
   return (
     <S.Section>
       <S.Category>
-        <S.Ul>
-          {MYPAGE_CATEGORY.map(({ title }, id) => (
-            <S.Li onClick={Li} key={id} id={id}>
-              {title}
-            </S.Li>
-          ))}
-        </S.Ul>
+        {MYPAGE_CATEGORY.map(({ title }, id) => {
+          return (
+            <List key={id}>
+              <ListItemButton
+                sx={{
+                  display: 'flex',
+                  height: '10vh',
+                  width: '80%',
+                  marginLeft: '10%',
+                  border: 'none',
+                  borderRadius: 5,
+                  color: 'rgba(0, 0, 0, 0.7)',
+                  backgroundColor: '#c0b4d6',
+                }}
+                onClick={() => handleCategory(id)}
+              >
+                <ListItemText
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                  }}
+                >
+                  {title}
+                </ListItemText>
+              </ListItemButton>
+            </List>
+          );
+        })}
       </S.Category>
       <S.Content>{ContentsComponents[id]}</S.Content>
     </S.Section>
--- src/pages/Mypage/Mypage.style.js
+++ src/pages/Mypage/Mypage.style.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Section = styled.div`
   display: flex;
   flex-direction: row;
+  height: 80vh;
   width: 100%;
   padding-top: 140px;
 `;
@@ -11,26 +12,10 @@ export const Category = styled.div`
   display: flex;
   flex-direction: column;
   width: 30%;
-  border: 1px solid black;
-`;
-
-export const Ul = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10%;
-  list-style: none;
-`;
-export const Li = styled.div`
-  font-size: 30px;
-  margin-bottom: 10%;
-  border: 1px solid black;
-  padding: 20px 20px;
+  justify-content: center;
 `;
 
 export const Content = styled.div`
   display: flex;
   width: 70%;
-  border: 1px solid black;
 `;
