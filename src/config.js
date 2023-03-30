a06282222e43976c44b6b3690934778e77e33db6
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-30 17:12:06 +0900

    Add: API 변수 추가

--- src/config.js
+++ src/config.js
@@ -1,1 +1,13 @@
-export const BASE_URL = '';
+const BASE_URL = 'http://10.58.52.60:3000';
+
+export const API = {
+  MAIN: `${BASE_URL}/`,
+  USERS: `${BASE_URL}/users`,
+  BIDS: `${BASE_URL}/bids`,
+  PURCHASES: `${BASE_URL}/payments/purchases`,
+  SALES: `${BASE_URL}/payments/sales`,
+  WISHLIST: `${BASE_URL}/wishlist`,
+  ORDERS: `${BASE_URL}/orders`,
+  KAKAO: `${BASE_URL}/users/auth/kakao/callback`,
+  ITEMS: `${BASE_URL}/items`,
+};
