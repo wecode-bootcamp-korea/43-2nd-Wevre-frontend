688441ba4279dd68a9de0ac921e33fce60bd1d42
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-04-04 05:08:48 +0900

    Add: 카카오페이 API 변수 추가

--- src/config.js
+++ src/config.js
@@ -12,4 +12,5 @@ export const API = {
   ORDERS: `${BASE_URL}/orders`,
   KAKAO: `${BASE_URL}/users/auth/kakao/callback`,
   ITEMS: `${BASE_URL}/items`,
+  KAKAOPAY: `${BASE_URL}/payment/kakao/ready`,
 };
