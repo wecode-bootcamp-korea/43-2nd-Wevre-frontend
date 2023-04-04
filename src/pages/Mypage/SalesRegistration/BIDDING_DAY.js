ebfd5616636759510b9d277ff41811e9ccfae868
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-04-05 03:55:53 +0900

    수정중
    수정중
    
    수정즁
    
    axios
    
    수정중

---
+++ src/pages/Mypage/SalesRegistration/BIDDING_DAY.js
@@ -0,0 +1,30 @@
+export const BIDDING_DAY = [
+  {
+    id: 1,
+    day: '1',
+  },
+  {
+    id: 2,
+    day: '2',
+  },
+  {
+    id: 3,
+    day: '3',
+  },
+  {
+    id: 4,
+    day: '4',
+  },
+  {
+    id: 5,
+    day: '5',
+  },
+  {
+    id: 6,
+    day: '6',
+  },
+  {
+    id: 7,
+    day: '7',
+  },
+];
---
+++ src/pages/Mypage/SalesRegistration/SalesRegistration.style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const CardBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const CardText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 80%;
+  margin: 10%;
+  line-height: 30px;
+`;
+
+export const CategoryTitle = styled.h1`
+  font-size: 26px;
+  margin-bottom: 10px;
+`;
+
+export const Title = styled.h1`
+  font-size: 20px;
+  margin-bottom: 30px;
+`;
+
+export const SubTitle = styled.h1`
+  opacity: 0.5;
+  margin-bottom: 10px;
+`;
+
+export const TextBox = styled.div`
+  margin-top: 20px;
+`;
