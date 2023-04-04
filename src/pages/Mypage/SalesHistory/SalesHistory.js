764acc10bc420e0c62a3e1c561dde8650d069826
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-04-04 22:38:36 +0900

    Add: 판매내역 레이아웃 및 통신 구성완료

--- src/pages/Mypage/SalesHistory/SalesHistory.js
+++ src/pages/Mypage/SalesHistory/SalesHistory.js
@@ -1,8 +1,84 @@
 import React from 'react';
+import { API } from '../../../config';
+import useFetch from '../../../hooks/useFetch';
+import { Card, CardMedia } from '@mui/material';
 import * as S from './SalesHistory.style';
 
 const SalesHistory = () => {
-  return <div>SalesHistory</div>;
+  const { loading, data } = useFetch(API.SALES);
+  if (loading) return <div>Loading</div>;
+  const salesData = data.sales;
+
+  return (
+    <S.SalesHistoryContainer>
+      <S.CategoryTitle>판매내역</S.CategoryTitle>
+      {salesData.map(items => {
+        const {
+          orderId,
+          imageUrl,
+          itemName,
+          authorName,
+          sellerName,
+          price,
+          shipmentFee,
+          totalPrice,
+          orderStatus,
+          ordersCreatedAt,
+          paymentCreatedAt,
+        } = items;
+
+        const orderPaymentInfo = [
+          { id: 1, name: '결제상태', 결제상태: orderStatus },
+          { id: 2, name: '판매자이름', 판매자이름: sellerName },
+          { id: 3, name: '낙찰가', 낙찰가: price },
+          { id: 4, name: '배송비', 배송비: shipmentFee },
+          { id: 5, name: '결제가격', 결제가격: totalPrice },
+        ];
+
+        const orderCreateAt = [
+          { id: 1, name: '주문시각', 주문시각: ordersCreatedAt },
+          { id: 2, name: '결제시각', 결제시각: paymentCreatedAt },
+        ];
+
+        return (
+          <S.CardBox key={orderId}>
+            <Card sx={{ display: 'flex', width: '100%' }}>
+              <S.ItemBox>
+                <CardMedia sx={{ width: 300, height: 300 }} image={imageUrl} />
+                <S.Title>{itemName}</S.Title>
+                <S.SubTitle>{authorName}</S.SubTitle>
+              </S.ItemBox>
+              <S.ItemInfoBox>
+                <S.OrderDetail>
+                  <S.Title>결제 정보</S.Title>
+                  {orderPaymentInfo.map(list => {
+                    const { id, name } = list;
+                    return (
+                      <S.SubTitle
+                        key={id}
+                      >{`${name} : ${list[name]}`}</S.SubTitle>
+                    );
+                  })}
+                </S.OrderDetail>
+                <S.OrderDetail>
+                  <S.CreateAtBox>
+                    {orderCreateAt.map(list => {
+                      const { id, name } = list;
+                      return (
+                        <S.CreateAt
+                          key={id}
+                        >{`${name} : ${list[name]}`}</S.CreateAt>
+                      );
+                    })}
+                  </S.CreateAtBox>
+                </S.OrderDetail>
+              </S.ItemInfoBox>
+            </Card>
+          </S.CardBox>
+        );
+      })}
+    </S.SalesHistoryContainer>
+  );
 };
 
 export default SalesHistory;
--- src/pages/Mypage/SalesHistory/SalesHistory.style.js
+++ src/pages/Mypage/SalesHistory/SalesHistory.style.js
@@ -1,1 +1,58 @@
 import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+export const SalesHistoryContainer = styled.div`
+  ${theme.mixins.flexDirection('column')}
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const CardBox = styled.div`
+  display: flex;
+  width: 100%;
+  margin-bottom: 100px;
+`;
+
+export const CategoryTitle = styled.h1`
+  font-size: 30px;
+  margin-bottom: 30px;
+`;
+
+export const ItemBox = styled.div`
+  ${theme.mixins.flexDirection('column')}
+  align-items: center;
+  padding: 50px;
+`;
+
+export const Title = styled.h1`
+  font-size: 20px;
+  margin: 20px 0px 20px 0px;
+`;
+
+export const SubTitle = styled.h1`
+  font-size: 16px;
+  opacity: 0.8;
+`;
+
+export const CreateAtBox = styled.h1`
+  margin-top: 300px;
+`;
+
+export const CreateAt = styled.h1`
+  font-size: 12px;
+  line-height: 20px;
+  opacity: 0.3;
+`;
+
+export const ItemInfoBox = styled.div`
+  display: flex;
+  width: 100%;
+  line-height: 30px;
+`;
+
+export const OrderDetail = styled.div`
+  ${theme.mixins.flexDirection('column')}
+  width: 50%;
+  padding: 30px;
+`;
