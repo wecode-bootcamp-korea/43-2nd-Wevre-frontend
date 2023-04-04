a400741c5fbca6d29be9b1e119a0e3240d36fa3a
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-04-05 03:55:53 +0900

    ADD : 상품 등록 페이지 구현 MODIFY : 상품 등록 데이터 통신

---
+++ src/pages/Mypage/SalesRegistration/ITEM_INPUT_INFO.js
@@ -0,0 +1,36 @@
+export const ITEM_INPUT_INFO = [
+  {
+    id: 1,
+    info: '작품명',
+    name: 'itemName',
+  },
+  {
+    id: 2,
+    info: '작가명',
+    name: 'authorName',
+  },
+  {
+    id: 3,
+    info: '무게',
+    unit: 'kg',
+    name: 'weight',
+  },
+  {
+    id: 4,
+    info: '가로',
+    unit: 'cm',
+    name: 'width',
+  },
+  {
+    id: 5,
+    info: '세로',
+    unit: 'cm',
+    name: 'length',
+  },
+  {
+    id: 6,
+    info: '높이',
+    unit: 'cm',
+    name: 'height',
+  },
+];
