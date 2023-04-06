e9ee55b3e44ca0f1f4e8f550e843683b832bdd29
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-04-06 14:11:09 +0900

    스타일수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,7 +2,15 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-    ${reset}
+    ${reset};
+    * {
+        color: rgba(0,0,0,0.8);
+    }
+    body {
+        background-color: rgb(245, 245, 247);
+        
+    }
+    
 `;
 
 export default GlobalStyle;
