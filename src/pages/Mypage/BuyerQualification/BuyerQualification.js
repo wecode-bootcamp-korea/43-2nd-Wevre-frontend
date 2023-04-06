64009652826d946da1d67384b15b1d7330e7b456
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-04-06 13:56:51 +0900

    wip"

--- src/pages/Mypage/BuyerQualification/BuyerQualification.js
+++ src/pages/Mypage/BuyerQualification/BuyerQualification.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { API } from '../../../config';
-import { Button } from '@mui/material';
+import { Card } from '@mui/material';
+import { Button, Box } from '@mui/joy';
 import { QUAL_INFO } from './QUAL_INFO';
 import * as S from './BuyerQualification.style';
 
@@ -8,10 +9,6 @@ const BuyerQualification = () => {
   const handleAgreeButton = () => {
     fetch(API.BUYER, {
       method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-        Authorization: localStorage.getItem('login-token'),
-      },
     })
       .then(res => res.json())
       .then(data => console.log(data));
@@ -19,27 +16,40 @@ const BuyerQualification = () => {
 
   return (
     <S.BuyerSection>
-      <S.Title>경매 입찰 자격 취득</S.Title>
-      <S.BuyerTextBox>
-        {QUAL_INFO.map(({ title, subTitle, id }) => (
-          <React.Fragment key={id}>
-            <S.BuyerTitle>{title}</S.BuyerTitle>
-            <S.BuyerSubText>{subTitle}</S.BuyerSubText>
-          </React.Fragment>
-        ))}
-      </S.BuyerTextBox>
-      <Button
-        onClick={() => handleAgreeButton()}
+      <Card
         sx={{
-          height: 50,
-          width: 150,
-          fontSize: 15,
-          marginBottom: '100px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '20px',
+          width: '100%',
+          marginBottom: '50px',
         }}
-        variant="contained"
       >
-        동의
-      </Button>
+        <S.Title>경매 입찰 자격 취득</S.Title>
+        <S.BuyerTextBox>
+          {QUAL_INFO.map(({ title, subTitle, id }) => (
+            <React.Fragment key={id}>
+              <S.BuyerTitle>{title}</S.BuyerTitle>
+              <S.BuyerSubText>{subTitle}</S.BuyerSubText>
+            </React.Fragment>
+          ))}
+        </S.BuyerTextBox>
+        <Box
+          sx={{
+            margin: '50px',
+          }}
+        >
+          <Button
+            size="lg"
+            onClick={() => handleAgreeButton()}
+            color="neutral"
+            variant="solid"
+          >
+            동 의
+          </Button>
+        </Box>
+      </Card>
     </S.BuyerSection>
   );
 };
