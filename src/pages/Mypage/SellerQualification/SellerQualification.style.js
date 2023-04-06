c7b0a4b1de90f9fcb411a9d1dc35a0c6f88277f0
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-04-06 10:23:18 +0900

    스타일수정

--- src/pages/Mypage/SellerQualification/SellerQualification.style.js
+++ src/pages/Mypage/SellerQualification/SellerQualification.style.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
-import theme from '../../../styles/theme';
 
 export const SellerQualContainer = styled.div`
-  ${theme.mixins.flexBox('center', 'center')}
-  flex-direction: column;
   width: 100%;
 `;
 
 export const Title = styled.h1`
+  margin: 5vh;
   font-size: 26px;
   margin-bottom: 50px;
 `;
